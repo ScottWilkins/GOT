fa682661e2eb91ff57e070f52f634ab176984d15
Scott Wilkins <scottwilkins@Scotts-mac.local>
2016-06-26 22:01:51 -0600

    final

--- logic.js
+++ logic.js
@@ -1,105 +1,177 @@
-function splashPageInit(){
-var graph = {
-  "nodes": [
-    {"x": 469, "y": 410},
-    {"x": 493, "y": 364},
-    {"x": 442, "y": 365},
-    {"x": 467, "y": 314},
-    {"x": 477, "y": 248},
-    {"x": 425, "y": 207},
-    {"x": 402, "y": 155},
-    {"x": 369, "y": 196},
-    {"x": 350, "y": 148},
-    {"x": 539, "y": 222},
-    {"x": 594, "y": 235},
-    {"x": 582, "y": 185},
-    {"x": 633, "y": 200}
-  ],
-  "links": [
-    {"source":  0, "target":  1},
-    {"source":  1, "target":  2},
-    {"source":  2, "target":  0},
-    {"source":  1, "target":  3},
-    {"source":  3, "target":  2},
-    {"source":  3, "target":  4},
-    {"source":  4, "target":  5},
-    {"source":  5, "target":  6},
-    {"source":  5, "target":  7},
-    {"source":  6, "target":  7},
-    {"source":  6, "target":  8},
-    {"source":  7, "target":  8},
-    {"source":  9, "target":  4},
-    {"source":  9, "target": 11},
-    {"source":  9, "target": 10},
-    {"source": 10, "target": 11},
-    {"source": 11, "target": 12},
-    {"source": 12, "target": 10}
-  ]
-}
+function splashPageInit() {
+    var graph = {
+        "nodes": [{
+            "x": 469,
+            "y": 410
+        }, {
+            "x": 493,
+            "y": 364
+        }, {
+            "x": 442,
+            "y": 365
+        }, {
+            "x": 467,
+            "y": 314
+        }, {
+            "x": 477,
+            "y": 248
+        }, {
+            "x": 425,
+            "y": 207
+        }, {
+            "x": 402,
+            "y": 155
+        }, {
+            "x": 369,
+            "y": 196
+        }, {
+            "x": 350,
+            "y": 148
+        }, {
+            "x": 539,
+            "y": 222
+        }, {
+            "x": 594,
+            "y": 235
+        }, {
+            "x": 582,
+            "y": 185
+        }, {
+            "x": 633,
+            "y": 200
+        }],
+        "links": [{
+            "source": 0,
+            "target": 1
+        }, {
+            "source": 1,
+            "target": 2
+        }, {
+            "source": 2,
+            "target": 0
+        }, {
+            "source": 1,
+            "target": 3
+        }, {
+            "source": 3,
+            "target": 2
+        }, {
+            "source": 3,
+            "target": 4
+        }, {
+            "source": 4,
+            "target": 5
+        }, {
+            "source": 5,
+            "target": 6
+        }, {
+            "source": 5,
+            "target": 7
+        }, {
+            "source": 6,
+            "target": 7
+        }, {
+            "source": 6,
+            "target": 8
+        }, {
+            "source": 7,
+            "target": 8
+        }, {
+            "source": 9,
+            "target": 4
+        }, {
+            "source": 9,
+            "target": 11
+        }, {
+            "source": 9,
+            "target": 10
+        }, {
+            "source": 10,
+            "target": 11
+        }, {
+            "source": 11,
+            "target": 12
+        }, {
+            "source": 12,
+            "target": 10
+        }]
+    }
 
-var width = 1450;
+    var width = 1450;
     height = 593;
 
-var force = d3.layout.force()
-    .size([width, height])
-    .charge(-1700)
-    .linkDistance(250)
-    .on("tick", tick);
+    var force = d3.layout.force()
+        .size([width, height])
+        .charge(-1700)
+        .linkDistance(250)
+        .on("tick", tick);
 
-var drag = force.drag()
-    .on("dragstart", dragstart);
- //var svg = d3.select("body").insert("svg",":first-child")
-var svg = d3.select(".main").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("id", "svg");
+    var drag = force.drag()
+        .on("dragstart", dragstart);
+    //var svg = d3.select("body").insert("svg",":first-child")
+    var svg = d3.select(".main").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("id", "svg");
 
     svg.append("defs").append("pattern")
-    .attr("id","image")
-    .attr("patternUnits","userSpaceOnUse")
-    .attr("height", "605")
-    .attr("width","2700")
-    .append("image").attr("x","-650")
-    .attr("y","-10").attr("height","605")
-    .attr("width","2700")
-    .attr("xlink:href","./resources/GOT-sigils.png")
+        .attr("id", "image")
+        .attr("patternUnits", "userSpaceOnUse")
+        .attr("height", "605")
+        .attr("width", "2700")
+        .append("image").attr("x", "-650")
+        .attr("y", "-10").attr("height", "605")
+        .attr("width", "2700")
+        .attr("xlink:href", "./resources/GOT-sigils.png")
 
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
+    var link = svg.selectAll(".link"),
+        node = svg.selectAll(".node");
 
 
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
 
-  link = link.data(graph.links)
-    .enter().append("line")
-      .attr("class", "link");
+    link = link.data(graph.links)
+        .enter().append("line")
+        .attr("class", "link");
 
-  node = node.data(graph.nodes)
-    .enter().append("circle")
-      .attr("class", "node")
-      .attr("r", 100)
-      .attr("fill","url(#image)")
-      .on("dblclick", dblclick)
-      .call(drag);
+    node = node.data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 100)
+        .attr("fill", "url(#image)")
+        .on("dblclick", dblclick)
+        .call(drag);
 
-function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+    function tick() {
+        link.attr("x1", function(d) {
+                return d.source.x;
+            })
+            .attr("y1", function(d) {
+                return d.source.y;
+            })
+            .attr("x2", function(d) {
+                return d.target.x;
+            })
+            .attr("y2", function(d) {
+                return d.target.y;
+            });
 
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-}
+        node.attr("cx", function(d) {
+                return d.x;
+            })
+            .attr("cy", function(d) {
+                return d.y;
+            });
+    }
 
-function dblclick(d) {
-  d3.select(this).classed("fixed", d.fixed = false);
-}
+    function dblclick(d) {
+        d3.select(this).classed("fixed", d.fixed = false);
+    }
 
-function dragstart(d) {
-  d3.select(this).classed("fixed", d.fixed = true);
-}
+    function dragstart(d) {
+        d3.select(this).classed("fixed", d.fixed = true);
+    }
 }
--- script.js
+++ script.js
@@ -1,77 +1,78 @@
-
 var housePics = {
-  362:"./resources/stark-house.jpg",
-  395:"./resources/tully-house.jpg",
-  380:"./resources/tarth-house.jpg",
-  72:"./resources/clegane-house.jpg",
-  202:"./resources/hornwood-house.jpg",
-  66:"./resources/cerwyn-house.jpg",
-  255:"./resources/manderly-house.jpg",
-  133:"./resources/florent-house.jpg",
-  7:"./resources/arryn-house.jpg",
-  10:"./resources/baelish-house.jpg",
-  427:"./resources/whent-house.jpg",
-  229:"./resources/lannister-house.jpg",
-  258:"./resources/marbrand-house.jpg",
-  317:"./resources/redwyne-house.jpg",
-  271:"./resources/mormont-house.jpg",
-  98:"./resources/dayne-house.jpg",
-  150:"./resources/glover-house.jpg",
-  169:"./resources/greyjoy-house.jpg",
-  215:"./resources/karstark-house.jpg",
-  34:"./resources/bolton-house.jpg",
-  117:"./resources/estermont-house.jpg",
-  285:"./resources/martell-house.jpg",
-  16:"./resources/baratheon-house.jpg",
-  102:"./resources/dondarrion-house.jpg",
-  80:"./resources/connington-house.jpg",
-  378:"./resources/targaryen-house.jpg",
-  398:"./resources/tyrell-house.jpg"
+    362: "./resources/stark-house.jpg",
+    395: "./resources/tully-house.jpg",
+    380: "./resources/tarth-house.jpg",
+    72: "./resources/clegane-house.jpg",
+    202: "./resources/hornwood-house.jpg",
+    66: "./resources/cerwyn-house.jpg",
+    255: "./resources/manderly-house.jpg",
+    133: "./resources/florent-house.jpg",
+    7: "./resources/arryn-house.jpg",
+    10: "./resources/baelish-house.jpg",
+    427: "./resources/whent-house.jpg",
+    229: "./resources/lannister-house.jpg",
+    258: "./resources/marbrand-house.jpg",
+    317: "./resources/redwyne-house.jpg",
+    271: "./resources/mormont-house.jpg",
+    98: "./resources/dayne-house.jpg",
+    150: "./resources/glover-house.jpg",
+    169: "./resources/greyjoy-house.jpg",
+    215: "./resources/karstark-house.jpg",
+    34: "./resources/bolton-house.jpg",
+    117: "./resources/estermont-house.jpg",
+    285: "./resources/martell-house.jpg",
+    16: "./resources/baratheon-house.jpg",
+    102: "./resources/dondarrion-house.jpg",
+    80: "./resources/connington-house.jpg",
+    378: "./resources/targaryen-house.jpg",
+    398: "./resources/tyrell-house.jpg"
 }
 
-  //one ajax call function to rule them all
-  function callAjax(id, url, func, type, optionalInfo) {
+//one ajax call function to rule them all
+function callAjax(id, url, func, type, optionalInfo) {
     $.ajax({
-            type: "GET",
-            dataType: type,
-            url: url + id,
-            beforeSend: function(xhr){xhr.setRequestHeader('Accept', 'application/json');}
-       }).done(function(result){
-         func(result, optionalInfo);
-     })
-   }
-  //clear the stage
-  function removeSplash(){
-      $('.main').html("")
-      $('svg').remove();
-      $('.main').addClass("container")
-      var div = document.createElement('div')
-      div.className = "house-container"
-      $('.main').append(div)
-  }
-  //show house name and list the sworn members of a house
-  //and if they have an actor, show that picture
-  function createHouse(data, id){
+        type: "GET",
+        dataType: type,
+        url: url + id,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Accept', 'application/json');
+        }
+    }).done(function(result) {
+        func(result, optionalInfo);
+    })
+}
+//clear the stage
+function removeSplash() {
+    $('.main').html("")
+    $('svg').remove();
+    $('.main').addClass("container")
+    var div = document.createElement('div')
+    div.className = "house-container"
+    $('.main').append(div)
+}
+//show house name and list the sworn members of a house
+//and if they have an actor, show that picture
+function createHouse(data, id) {
     removeSplash();
     var houseName = data.name;
     var spacer = document.createElement('div')
     var h3 = document.createElement("h3")
     h3.className = 'h3Houses'
     var ul = document.createElement('ul')
-    $(spacer).css( "height", "90px" );
+    $(spacer).css("height", "90px");
     ul.className = ('swornMembers')
     $(h3).html(houseName)
     $('.main').css('background-image', 'url(' + housePics[id] + ')')
     $('.house-container').append(h3).append(spacer)
-     .append(ul)
-  //   data.swornMembers.forEach(function(member) {callAjax("", member, createMemberArray, 'json')
-  //  });
-  for (let member of data.swornMembers){
-    callAjax("", member, createMemberArray)
-  }
-  }
+        .append(ul)
+        //   data.swornMembers.forEach(function(member) {callAjax("", member, createMemberArray, 'json')
+        //  });
+    for (let member of data.swornMembers) {
+        callAjax("", member, createMemberArray)
+    }
+}
 
-  function createMemberArray(data){
+function createMemberArray(data) {
     var swornMember = data.name;
     var li = document.createElement('li')
     var span = document.createElement('span')
@@ -79,60 +80,64 @@ var housePics = {
     li.id = data.url
     $(li).html(swornMember).appendTo(span)
     $('.swornMembers').append(span)
-  }
+}
 
-  function createActorPic(data, options){
+function createActorPic(data, options) {
     var div = options[0]
     var actor = options[1]
     var img = document.createElement('img')
     img.className = "actorImg"
-    if(data.results.length > 0 &&
-      (data.results[0].profile_path ? data.results[0].profile_path!==null : false)){//data.results[0].profile_path !== null){
-      var id = data.results[0].profile_path;
-      $(img).attr("src", `https://image.tmdb.org/t/p/w1280/`+id)
+    if (data.results.length > 0 &&
+        (data.results[0].profile_path ? data.results[0].profile_path !== null : false)) { //data.results[0].profile_path !== null){
+        var id = data.results[0].profile_path;
+        $(img).attr("src", `https://image.tmdb.org/t/p/w1280/` + id)
     } else {
-      $(img).attr("src", "./resources/no-picture.png")
+        $(img).attr("src", "./resources/no-picture.png")
     }
-    $(img).css("height","280px")
-    .css("width", "200px")
+    $(img).css("height", "280px")
+        .css("width", "200px")
     $(div).append(img)
-  }
+}
 
-  function createMemberIframeEvent() {
+function createMemberIframeEvent() {
     $('.nameInfo').remove()
     $('iframe').remove()
-    $('.main').css("background-image","none")
+    $('.main').css("background-image", "none")
     var name = $(this).text();
     $('<iframe>', {
-       src: 'http://awoiaf.westeros.org/index.php/' + name,
-       class:  'iFrame',
-       frameborder: 0,
-    })
-      .appendTo('.main');
-  }
-  function createHouseEvent(func) {
+            src: 'http://awoiaf.westeros.org/index.php/' + name,
+            class: 'iFrame',
+            frameborder: 0,
+        })
+        .appendTo('.main');
+}
+
+function createHouseEvent(func) {
 
     var id = this.id;
     callAjax(id, 'http://www.anapioficeandfire.com/api/houses/', createHouse, 'json', id)
-  }
-  function showHouseInfo() {
+}
+
+function showHouseInfo() {
     var id = this.id;
-      $('iframe').hide()
-      callAjax(id, 'http://www.anapioficeandfire.com/api/houses/', houseInfo, 'json', id)
+    $('iframe').hide()
+    callAjax(id, 'http://www.anapioficeandfire.com/api/houses/', houseInfo, 'json', id)
 
-  }
-  function hideHouseInfo(){
-        $('.nameInfo').remove()
-        $('iframe').fadeIn("fast")
+}
 
-  }
-  function houseInfo(data, id){
+function hideHouseInfo() {
+    $('.nameInfo').remove()
+    $('iframe').fadeIn("fast")
+
+}
+
+function houseInfo(data, id) {
     var founded = data.founded || "Long, long ago"
     var words = data.words || "Data not found, but surely they are epic"
     var houseName = data.name
     var houseRegion = data.region
     var coatOfArms = data.coatOfArms
-    var houseText =  document.createElement('div')
+    var houseText = document.createElement('div')
     var nameDiv = document.createElement('div')
     var img = document.createElement('img')
     var imgDiv = document.createElement('div')
@@ -140,35 +145,39 @@ var housePics = {
     textDiv.className = "textDiv"
     nameDiv.className = "nameInfo"
     $(img).attr("src", housePics[id]).
-      attr("height", "595px")
-    img.setAttribute("style","-webkit-filter:brightness(180%)")
+    attr("height", "595px")
+    img.setAttribute("style", "-webkit-filter:brightness(180%)")
     $(houseText).append(`<h3>${houseName}</h3>`).
-      append(`<h4>Region:  ${houseRegion}</h4>`).
-      //append(`<h4>Coat of Arms:  ${coatOfArms}</h4>`).
-      append(`<h4>Words:  ${words}</h4>`).
-      append(`<h4>Founded:  ${founded}</h4>`)
+    append(`<h4>Region:  ${houseRegion}</h4>`).
+        //append(`<h4>Coat of Arms:  ${coatOfArms}</h4>`).
+    append(`<h4>Words:  ${words}</h4>`).
+    append(`<h4>Founded:  ${founded}</h4>`)
     $(imgDiv).append(img)
     $(textDiv).append(houseText)
     $(nameDiv).append(imgDiv).
-      append(textDiv).
-      hide()
+    append(textDiv).
+    hide()
     $('.main').append(nameDiv)
     $(nameDiv).fadeIn("fast")
-  }
-  function htmlIfy(str){
-    return str.replace(" ","%20")
-  }
-  function hideMemberInfo(){
-        $('.nameInfo').fadeOut("fast").
-        remove()
-        $('iframe').show()
-  }
-  function showMemberInfo() {
+}
+
+function htmlIfy(str) {
+    return str.replace(" ", "%20")
+}
+
+function hideMemberInfo() {
+    $('.nameInfo').fadeOut("fast").
+    remove()
+    $('iframe').show()
+}
+
+function showMemberInfo() {
     $('iframe').hide()
     var id = $(this).attr("id");
-      callAjax("", id, memberInfo, 'json')
-  }
-  function memberInfo(data){
+    callAjax("", id, memberInfo, 'json')
+}
+
+function memberInfo(data) {
     var name = data.name
     var born = data.born || "Age of Winter"
     var died = data.died || "Alive (for now)"
@@ -176,7 +185,7 @@ var housePics = {
     var titlesArray = data.titles[0] || ["Inhabitant of Westoros"]
     var gender = data.gender
     var playedBy = data.playedBy[0] || "Not listed for the HBO series"
-    var nameText =  document.createElement('h2')
+    var nameText = document.createElement('h2')
     var nameDiv = document.createElement('div')
     var img = document.createElement('img')
     var imgDiv = document.createElement('div')
@@ -184,41 +193,41 @@ var housePics = {
     var pic;
     textDiv.className = "textDiv"
     nameDiv.className = "nameInfo"
-    pic = gender === "Male" ?  "./resources/guy-tall.png" : "./resources/girl-tall.png"
+    pic = gender === "Male" ? "./resources/guy-tall.png" : "./resources/girl-tall.png"
     $(img).attr("src", pic).
-      attr("height", "595px")
+    attr("height", "595px")
     $(nameText).html(name).
-      append(`<h4>Title:  ${titlesArray}</h4>`).
-      append(`<h4>Culture:  ${culture}</h4>`).
-      append(`<h4>born:  ${born}</h4>`).
-      append(`<h4>died:  ${died}</h4>`).
-      append(`<h4>Played by:  ${playedBy}</h4>`)
+    append(`<h4>Title:  ${titlesArray}</h4>`).
+    append(`<h4>Culture:  ${culture}</h4>`).
+    append(`<h4>born:  ${born}</h4>`).
+    append(`<h4>died:  ${died}</h4>`).
+    append(`<h4>Played by:  ${playedBy}</h4>`)
     $(imgDiv).append(img)
     $(textDiv).append(nameText)
     $(nameDiv).append(imgDiv).
-      append(textDiv)
-      if(playedBy !== "Not listed for the HBO series"){
-        callAjax(htmlIfy(playedBy),'https://api.themoviedb.org/3/search/person?api_key=6d797f03997e4b6fa4035391d0ebb660&query=',
-                createActorPic, 'jsonp', [textDiv, playedBy])
-              }
+    append(textDiv)
+    if (playedBy !== "Not listed for the HBO series") {
+        callAjax(htmlIfy(playedBy), 'https://api.themoviedb.org/3/search/person?api_key=6d797f03997e4b6fa4035391d0ebb660&query=',
+            createActorPic, 'jsonp', [textDiv, playedBy])
+    }
     $(nameDiv).hide()
     $('.main').append(nameDiv)
     $(nameDiv).fadeIn("fast")
 
-  }
+}
 
-  $(document).ready(function(){
-     splashPageInit()
-     $('.sigil').on("click", createHouseEvent)
-     $(document).on('click', '.sworn-member', createMemberIframeEvent)
-     $(document).hoverIntent({
-         over: showHouseInfo,
-         out: hideHouseInfo,
-         selector: '.sigil'
-     });
-     $(document).hoverIntent({
-         over: showMemberInfo,
-         out: hideMemberInfo,
-         selector: '.sworn-member'
-     });
-  })
+$(document).ready(function() {
+    splashPageInit()
+    $('.sigil').on("click", createHouseEvent)
+    $(document).on('click', '.sworn-member', createMemberIframeEvent)
+    $(document).hoverIntent({
+        over: showHouseInfo,
+        out: hideHouseInfo,
+        selector: '.sigil'
+    });
+    $(document).hoverIntent({
+        over: showMemberInfo,
+        out: hideMemberInfo,
+        selector: '.sworn-member'
+    });
+})
